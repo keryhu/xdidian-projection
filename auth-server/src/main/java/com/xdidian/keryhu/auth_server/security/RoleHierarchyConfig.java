9d04f617bf0691548040ec27a674f859cfaa4797
keryhu <keryhu@hotmail.com>
2016-08-08 17:03:02 +0800

    update role Hierarchy

--- auth-server/src/main/java/com/xdidian/keryhu/auth_server/security/RoleHierarchyConfig.java
+++ auth-server/src/main/java/com/xdidian/keryhu/auth_server/security/RoleHierarchyConfig.java
@@ -4,21 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 
+import static com.xdidian.keryhu.util.Constants.ROLE_HIERARCHY;
 @Configuration
 public class RoleHierarchyConfig {
 
   /**
-   * 设定管理员>客服人员> 物业公司注册人员 或 物业租户
+   * 
+  * @Title: roleHierarchy
+  * @Description: TODO(系统用户role 排序管理)
+  * @param @return    设定文件
+  * @return RoleHierarchyImpl    返回类型
+  * @throws
    */
+  
   @Bean
   public RoleHierarchyImpl roleHierarchy() {
     RoleHierarchyImpl roleHierarchyImpl = new RoleHierarchyImpl();
-    // (设定管理员>客服人员> 物业公司注册人员 或 物业租户)
-    // new StringBuffer("")
-    roleHierarchyImpl.setHierarchy("ROLE_ADMIN>ROLE_SERVICE  ROLE_SERVICE>ROLE_PROPERTY  "
-        + "  ROLE_SERVICE>ROLE_TENANT  ROLE_SERVICE>ROLE_INTENTION ");
+    // 新地点管理员》新地点客服人员》客户管理员》客户部门管理员》默认人员
+    roleHierarchyImpl.setHierarchy(ROLE_HIERARCHY);
     return roleHierarchyImpl;
   }
 
-
 }
+
